57d3b7cf7c9256fea1e3f059380d1af380f93bbb
陈欢 <jingfeng_quietwind@163.com>
2017-06-03 21:21:55 +0800

    update item

--- index.js
+++ index.js
@@ -8,18 +8,40 @@ xhr.onreadystatechange = function () {
         }
     }
 };
-function groupItems(itemsData, gapTime) {
+function groupItems(itemsData, gapTime, isReal /* 是否要得到最终统计数据 */) {
+    if (isReal === void 0) { isReal = false; } /* 是否要得到最终统计数据 */
     var groupResult = [];
+    if (isReal) {
+        groupResult;
+    }
     var minTime = itemsData[0].timestamp;
     var maxTime = itemsData.slice(-1)[0].timestamp;
     itemsData.forEach(function (item) {
         // 计算在结果中的哪个位置
         var index = Math.floor((item.timestamp - minTime) / gapTime);
         var groupItem = groupResult[index];
-        if (!groupItem) {
-            groupResult[index] = [];
+        if (isReal) {
+            if (!groupItem) {
+                groupResult[index] = {
+                    success_count: 0,
+                    error_count: 0,
+                    page_per_second: 0,
+                    total_count: 0,
+                    left_count: 0
+                };
+            }
+            groupResult[index].success_count += item.success_count;
+            groupResult[index].error_count += item.error_count;
+            groupResult[index].page_per_second += item.page_per_second;
+            groupResult[index].total_count += item.total_count;
+            groupResult[index].left_count += item.left_count;
         }
-        groupResult[index].push(item);
+        else {
+            if (!groupItem) {
+                groupResult[index] = [];
+            }
+            groupResult[index].push(item);
+        }
     });
     return groupResult;
 }
@@ -30,6 +52,9 @@ function initData(data) {
         return a.timestamp > b.timestamp ? 1 : -1;
     });
     // 得到分组数据
-    var groups = groupItems(Items, 30 * 60);
+    var groups = groupItems(Items, 60 * 60);
     console.log(groups.length, groups);
+    // 得到分组统计数据
+    var groupsData = groupItems(Items, 60 * 60, true);
+    console.log(groupsData);
 }
--- index.ts
+++ index.ts
@@ -13,6 +13,14 @@ interface DataItem {
     left_count: number;
 }
 
+interface RealNums {
+    success_count: number;
+    error_count: number;
+    page_per_second: number;
+    total_count: number;
+    left_count: number;
+}
+
 interface Data{
     error_code: number;
     success: boolean;
@@ -28,8 +36,9 @@ xhr.onreadystatechange = function() {
   }
 };
 
-function groupItems(itemsData: DataItem[], gapTime: number) {
+function groupItems(itemsData: DataItem[], gapTime: number, isReal = false /* 是否要得到最终统计数据 */) {
     const groupResult = [];
+    if (isReal) { groupResult as RealNums[] }
     const minTime = itemsData[0].timestamp;
     const maxTime = itemsData.slice(-1)[0].timestamp;
 
@@ -37,9 +46,28 @@ function groupItems(itemsData: DataItem[], gapTime: number) {
         // 计算在结果中的哪个位置
         const index = Math.floor((item.timestamp - minTime) / gapTime)
         const groupItem = groupResult[index]
-        if (!groupItem) { groupResult[index] = [] }
+
+        if (isReal) {
+            if (!groupItem) {
+                groupResult[index] = {
+                    success_count: 0,
+                    error_count: 0,
+                    page_per_second: 0,
+                    total_count: 0,
+                    left_count: 0
+                }
+            }
+
+            groupResult[index].success_count += item.success_count
+            groupResult[index].error_count += item.error_count
+            groupResult[index].page_per_second += item.page_per_second
+            groupResult[index].total_count += item.total_count
+            groupResult[index].left_count += item.left_count
+        } else {
+            if (!groupItem) { groupResult[index] = [] }
+            groupResult[index].push(item)
+        }
         
-        groupResult[index].push(item)
     });
 
     return groupResult;
@@ -53,7 +81,12 @@ function initData(data: Data) {
     });
 
     // 得到分组数据
-    const groups = groupItems(Items, 30 * 60)
+    const groups = groupItems(Items, 60 * 60)
 
     console.log(groups.length, groups);
+    
+    // 得到分组统计数据
+    const groupsData = groupItems(Items, 60 * 60, true)
+    
+    console.log(groupsData)
 }
